c5987701355dc4145af7ace78ee9c85632fb00d4
kartik-sharma-09 <kartik.sharma210210@gmail.com>
2024-11-07 22:22:52 +0530

    adding routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 // import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
+import { Routes,Route } from "react-router-dom";
+import Home from './Pages/Home/Home'
+import Coin from './Pages/Coin/Coin'
 
-const App = () => {
+const App = () => { 
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/coin/:coinId" element={<Coin/>}/>
+      </Routes>
     </div>
   );
 };
