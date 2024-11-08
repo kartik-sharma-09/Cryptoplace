7d7c577e92383d68bf295b87d95c6aa70ac2757e
kartik-sharma-09 <kartik.sharma210210@gmail.com>
2024-11-08 23:12:06 +0530

    Add new files and modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import { Routes,Route } from "react-router-dom";
 import Home from './Pages/Home/Home'
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,35 @@
-// import React from "react";
+import React, { useContext } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import arrow_icon from "../../assets/arrow_icon.png";
+import { CoinContext } from "../../Context/coinContext";
 
 const Navbar = () => {
+  const { setCurrency } = useContext(CoinContext);  // allCoin,currency,setCurrency
+
+  const currencyHandler = (event) => {
+    switch (event.target.value) {
+      case "usd": {
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      }
+      case "eur": {
+        setCurrency({ name: "eur", symbol: "€" });
+        break;
+      }
+      case "inr": {
+        setCurrency({ name: "inr", symbol: "₹" });
+        break;
+      }
+      default: {
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      }
+    }
+  };
   return (
     <div className="navbar">
-      <img src={logo} alt="logo.png" className="logo"  />
+      <img src={logo} alt="logo.png" className="logo" />
       <ul>
         <li>Home</li>
         <li>Features</li>
@@ -14,12 +37,14 @@ const Navbar = () => {
         <li>Blog</li>
       </ul>
       <div className="nav-right">
-        <select>
+        <select onChange={currencyHandler}>
           <option value="usd">USD</option>
           <option value="eur">EUR</option>
-          <option value="INR">INR</option>
+          <option value="inr">INR</option>
         </select>
-        <button>Sign up <img src={arrow_icon}/></button>
+        <button>
+          Sign up <img src={arrow_icon} />
+        </button>
       </div>
     </div>
   );
